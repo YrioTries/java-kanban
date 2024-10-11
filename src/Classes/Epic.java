f75060bb58a422319f1aa4688abcb6f6738e2f4c
Балагур Дмитрий <farnitin@yandex.ru>
2024-10-11 14:45:32 +0400

    Debugs

--- src/Classes/Epic.java
+++ src/Classes/Epic.java
@@ -6,16 +6,11 @@ public class Epic extends Task{
     private HashMap<Integer, Subtask> epicSubtasks;
      public Epic(String title, String description){
          super(title, description);
-         epicSubtasks =  new HashMap();
+         epicSubtasks =  new HashMap<>();
      }
      public HashMap<Integer, Subtask> getSubMap(){
          return epicSubtasks;
      }
-
-     public void setEpicTasks(final Integer id, Subtask sup){
-         epicSubtasks.put(id, sup);
-     }
-
      public Subtask getSubTask(int id){
          return epicSubtasks.get(id);
      }
