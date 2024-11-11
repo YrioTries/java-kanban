4d4e21d6fe3fa3972c035477970ad73c9efc8b31
Balagur Dmitry <farnitin@yandex.ru>
2024-11-12 03:20:28 +0400

    Remove var size in handleLinkedMap

--- src/controllers/InMemoryHistoryManager.java
+++ src/controllers/InMemoryHistoryManager.java
@@ -59,26 +59,9 @@ class HandleLinkedHashMap {
     Map<Integer, Node> handleLinkedMap;
     private Node head;
     private Node tail;
-    private int size;
 
     public HandleLinkedHashMap() {
         handleLinkedMap = new HashMap<>();
-        size = 0;
-    }
-
-    public void addFirst(Task task) {
-        if (handleLinkedMap.containsKey(task.getId())){
-            removeNodeId(task.getId());
-        }
-        final Node oldHead = head;
-        final Node newNode = new Node(null, task, oldHead);
-        handleLinkedMap.put(task.getId(), newNode);
-        head = newNode;
-        if (oldHead == null)
-            tail = newNode;
-        else
-            oldHead.prev = newNode;
-        size++;
     }
 
     public void addLast(Task task) {
@@ -87,39 +70,34 @@ class HandleLinkedHashMap {
 
             Node prevMove = nodeNeedToMove.prev;
             Node nextMove = nodeNeedToMove.next;
-            if (prevMove != null && nextMove != null && size != 0){
-                prevMove.next = nextMove;
-                nextMove.prev = prevMove;
-            }
 
             if (nodeNeedToMove == head) {
                 head = nextMove;
             }
 
-            if (tail != null && size != 1) tail.next = nodeNeedToMove;
+            if (prevMove != null && nextMove != null){
+                prevMove.next = nextMove;
+                nextMove.prev = prevMove;
+            }
             nodeNeedToMove.prev = tail;
             nodeNeedToMove.next = null;
 
-            if (size == 0) {
-                head = tail = nodeNeedToMove;
-            } else {
-                tail = nodeNeedToMove;
-            }
+            tail.next = nodeNeedToMove;
+            tail = nodeNeedToMove;
 
             return;
         }
-        //final Node oldTail = tail;
+
         Node newNode = new Node(tail, task, null);
-        handleLinkedMap.put(task.getId(), newNode);
 
         if (tail != null) tail.next = newNode;
 
-        if (size == 0) {
+        if (size() == 0) {
             head = tail = newNode;
         } else {
             tail = newNode;
         }
-        size++;
+        handleLinkedMap.put(task.getId(), newNode);
     }
 
     public Task getFirst() {
@@ -149,11 +127,11 @@ class HandleLinkedHashMap {
         Node prevDel = needToDel.prev;
         Node AfterDel = needToDel.next;
 
-        if (prevDel != null && size != 0) {
+        if (prevDel != null && size() != 0) {
             prevDel.next = AfterDel;
         }
 
-        if (AfterDel != null && size != 0) {
+        if (AfterDel != null && size() != 0) {
             AfterDel.prev = prevDel;
         }
 
@@ -161,10 +139,10 @@ class HandleLinkedHashMap {
             head = AfterDel;
         }
 
-        if (position == size) {
+        if (position == size()) {
             tail = prevDel;
         }
-        size--;
+        handleLinkedMap.remove(needToDel.data.getId());
     }
 
     public void removeNodeId(int id) {
@@ -176,7 +154,7 @@ class HandleLinkedHashMap {
 
         handleLinkedMap.remove(id);
 
-        if (prevDel != null && AfterDel != null && size != 0) {
+        if (prevDel != null && AfterDel != null && size() != 0) {
             prevDel.next = AfterDel;
             AfterDel.prev = prevDel;
         }
@@ -189,11 +167,11 @@ class HandleLinkedHashMap {
             tail = prevDel;
         }
 
-        size--;
+        handleLinkedMap.remove(id);
     }
 
     public int size() {
-        return this.size;
+        return handleLinkedMap.size();
     }
 
     public ArrayList<Task> getTasks() {
@@ -210,11 +188,11 @@ class HandleLinkedHashMap {
 
     @Override
     public String toString() {
-        String res = "HandleLinkedList[size:" + size + "] [";
+        String res = "HandleLinkedList[size:" + size() + "] [";
         Node pr = head;
 
-        for (int i = 0; i <= size; i++){
-            if (i == size){
+        for (int i = 0; i <= size(); i++){
+            if (i == size()){
                 res = pr.data + ". ";
             } else {
                 res = pr.data + ", ";
